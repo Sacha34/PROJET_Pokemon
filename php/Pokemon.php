b8cadea2d8de4d23057d458d6c78325932f2049d
inosec2 <isecsploit@gmail.com>
2019-07-26 16:11:17 +0200

    pokemon

--- php/Pokemon.php
+++ php/Pokemon.php
@@ -5,21 +5,16 @@ class Pokemon
 {
     private $pok_id;
     private $pok_name;
-    private $vie_base;
+    private $vie_base=100;
     private $evolution;
     private $pok_xp;
     private $urlImage;
     private $abilities;
 
-    public function __construct(string $name,int $vie,array $abilities,int $evolution,string $urlImage, int $xp)
-    {
-        $this->pok_name=$name;
-        $this->pok_xp=$xp;
-        $this->vie_base=$vie;
-        $this->abilities=array($abilities);
-        $this->evolution=$evolution;
-        $this->urlImage=$urlImage;
-    }
+    private $attack_name;
+    private $damages=5;
+    private $attack_accuracy;
+    private $xp;
 
     public function getPokId():int{
         return $this->pok_id;
@@ -34,12 +29,30 @@ class Pokemon
         return $this;
     }
 
-    public function getVie():int{
-        return $this->vie_base;
+    public function getAttack():int{
+        return $this->attack_name;
     }
 
-    public function setVie($vie){
-        $this->vie_base=$vie;
+    public function setAttack($attack_name){
+        $this->attack_name=$attack_name;
+        return $this;
+    }
+
+    public function getDamages():int{
+        return $this->damages;
+    }
+
+    public function setDamages($damages){
+        $this->damages=$damages;
+        return $this;
+    }
+
+    public function getAccuracy():int{
+        return $this->attack_accuracy;
+    }
+
+    public function setAccuracy($accuracy){
+        $this->attack_accuracy=$accuracy;
         return $this;
     }
 
@@ -52,14 +65,35 @@ class Pokemon
         return $this;
     }
 
-    public function getXp():int{
-        return $this->pok_xp;
+    public function getXp(){
+        echo $this->xp."<br>";
+        return $this->xp;
     }
 
     public function setXp($xp){
-        $this->pok_xp=$xp;
+        $this->xp=$xp;
         return $this;
     }
+
+    public function getVie():int{
+        echo "La vie de " .$this->pok_name . ":". $this->vie_base . "<br>";
+        return $this->vie_base;
+    }
+
+    public function setVie($vie){
+        $this->vie_base=$vie;
+        return $this;
+    }
+
+    public function __construct(string $name,/*array $abilities,*/int $evolution,/*string $urlImage,*/ int $xp)
+    {
+        $this->pok_name=$name;
+        $this->pok_xp=$xp;
+        //$this->abilities=array($abilities);
+        $this->evolution=$evolution;
+        //$this->urlImage=$urlImage;
+    }
+
 
     public function getImage():string{
         return $this->urlImage;
@@ -79,5 +113,19 @@ class Pokemon
         echo "Ces attaques: " . array($abilities);
         echo "</pre>";
 
+    }
+
+    public function hit(Pokemon $defenseur){
+        if($this->vie_base <= 0){
+            echo $this->pok_name . "died!!";
+        }else{
+            $defenseur->vie_base -= $this->damages;
+            $this->winXp();
+            $this->setVie($this->vie_base);
+        }
+    }
+
+    public function winXp(){
+        $this->xp=$this->xp+1;
     }
 }
